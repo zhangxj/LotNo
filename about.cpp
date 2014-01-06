6c0a5cec0622ed1769f72951c58c40936c8b01c7
zhangxj <568628130@qq.com>
2014-01-07 01:13:47 +0800

    ..

--- about.cpp
+++
@@ -1,6 +0,0 @@
-#include "about.h"
-
-About::About(QQuickView *parent) :
-    QQuickView(parent)
-{
-}
--- qml/LotNo/func.js
+++
@@ -1,5 +0,0 @@
-
-
-function get_image_path(file_name){
-    return "../LotNo/images/" + file_name;
-}
